67ca1e46d1ed26cbecdecc49ec97da4281d65770
Ян Андреевич Худорожков <y.khudorozhkov@g.nsu.ru>
2022-10-10 17:23:00 +0000

    Edited src/main.cpp

--- lab1-BigInt/src/main.cpp
+++ lab1-BigInt/src/main.cpp
@@ -1,6 +1,4 @@
 #include "bigint.h"
-#include <bitset>
-#include <iostream>
 
 /**
  * ЯЧЕЙКА ХРАНИТ 9 СИМВОЛОВ \n
@@ -10,10 +8,5 @@
 
 int main() {
 
-    std::bitset<32> b1 (21847);
-    std::cout << b1 << std::endl;
-    std::bitset<32> b2 (-21930*(-1));
-    std::cout << b2 <<std::endl;
-    std::cout << std::bitset<32>(21847 | -21930);
     return 0;
 }
