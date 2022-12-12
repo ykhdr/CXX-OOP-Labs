368d28d9716d4b3c3b75d1a570d9239ad7998929
ykhudorozhkov <y.khudorozhkov@g.nsu.ru>
2022-12-13 04:13:28 +0700

    Added main logic and constructor CSVParser

--- lab4-CSV_Parser/lib/CSVParser/include/CSVParser.hpp
+++ lab4-CSV_Parser/lib/CSVParser/include/CSVParser.hpp
@@ -1,6 +1,67 @@
 #pragma once
 
+#include <limits>
+
+#include "TuplePrint.hpp"
+
+template<typename ...Types>
 class CSVParser
 {
+private:
+    class InputIterator;
 
+    std::ifstream &input_file_;
+
+    int line_offset_;
+
+    char column_delimiter_;
+    char line_delimiter_;
+    char escape_sym_;
+
+    int current_line_ = 0;
+
+public:
+    explicit CSVParser(
+            std::ifstream &input_file,
+            int line_offset = 0,
+            char column_delimiter = ',',
+            char line_delimiter = '\n',
+            char escape_sym = '\"') :
+            input_file_(input_file),
+            line_offset_(line_offset),
+            column_delimiter_(column_delimiter),
+            line_delimiter_(line_delimiter),
+            escape_sym_(escape_sym)
+    {
+        if (!input_file_.is_open())
+        {
+            throw std::ifstream::failure("File is not open");
+        }
+
+        for (current_line_; current_line_ < line_offset; ++current_line_)
+        {
+            if (!input_file_.ignore(std::numeric_limits<std::streamsize>::max(), input_file.widen(line_delimiter_)))
+            {
+                throw std::ifstream::failure("File does not contain " + std::to_string(line_offset_) + " lines");
+            }
+        }
+    }
+
+    CSVParser(const CSVParser &) = delete;
+
+    InputIterator begin()
+    {
+
+    }
+
+    InputIterator end()
+    {
+
+    }
+};
+
+template<typename ...Types>
+class CSVParser<Types...>::InputIterator
+{
+    
 };
--- lab4-CSV_Parser/src/main.cpp
+++ lab4-CSV_Parser/src/main.cpp
@@ -5,14 +5,74 @@
 
 int main(int argc, char const *argv[])
 {
-    std::tuple tp{1, "1", 10223.123, char(213)};
+    try
+    {
+        if (argc < 2 || argc > 6)
+        {
+            throw std::invalid_argument("Wrong number of parameters. Use -h or --help for help");
+        }
 
-    std::ofstream os{"text.txt"};
-    std::cout << tp;
-    os << tp;
+        std::ifstream file(argv[1]);
 
-    std::tuple tp1;
-    std::cout << tp1;
+        if (argc == 2)
+        {
+            CSVParser<int, std::string> parser(file);
+//                for (const auto &rs: parser)
+//                {
+//                    std::cout << rs << std::endl;
+//                }
+        }
+        else
+        {
+            int line_offset;
+            try
+            {
+                line_offset = std::stoi(argv[2]);
+            }
+            catch (const std::exception &ex)
+            {
+                throw std::invalid_argument("Bad line offset parameter :" + std::string(argv[2]));
+            }
 
-    return 0;
+            if (argc == 3)
+            {
+                CSVParser<int, std::string> parser(file, line_offset);
+//                for (const auto &rs: parser)
+//                {
+//                    std::cout << rs << std::endl;
+//                }
+            }
+            else if(argc == 4)
+            {
+                CSVParser<int, std::string> parser(file, line_offset,*argv[3]);
+//                for (const auto &rs: parser)
+//                {
+//                    std::cout << rs << std::endl;
+//                }
+            }
+            else if(argc == 5)
+            {
+                CSVParser<int, std::string> parser(file, line_offset,*argv[3],*argv[4]);
+//                for (const auto &rs: parser)
+//                {
+//                    std::cout << rs << std::endl;
+//                }
+            }
+            else if(argc == 6)
+            {
+                CSVParser<int, std::string> parser(file, line_offset, *argv[3], *argv[4], *argv[5]);
+//                for (const auto &rs: parser)
+//                {
+//                    std::cout << rs << std::endl;
+//                }
+            }
+        }
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
